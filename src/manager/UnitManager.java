0f3af93ed3fd0a27830859d4a6dbc3add1a1db4d
SG5143 <sun5143a@gmail.com>
2024-11-18 18:01:58 +0900

    Add random Monster generation method

--- src/manager/UnitManager.java
+++ src/manager/UnitManager.java
@@ -3,13 +3,20 @@ package manager;
 import java.util.ArrayList;
 import java.util.Random;
 
+import managers.UserDataManager;
 import units.Adventurer;
+import units.Monster;
 
 public class UnitManager {
 	public static Random r;
+	private static ArrayList<Monster> monsterList;
+
+	private final static String PATE = "manager.";
+	private final static String MONS[] = { "MonsterGhoul", "MonsterGoblin", "MonsterOrc" };
 
 	private UnitManager() {
 		UnitManager.r = new Random();
+		monsterList = new ArrayList<Monster>();
 	}
 
 	private static UnitManager instance = new UnitManager();
@@ -23,7 +30,7 @@ public class UnitManager {
 
 		while (true) {
 			int code = r.nextInt(999) + 1;
-			
+
 			boolean isFound = false;
 			for (Adventurer a : list) {
 				if (a.getCode() == code)
@@ -35,4 +42,24 @@ public class UnitManager {
 		}
 	}
 
+	public static ArrayList<Monster> getMonster() {
+		int num = r.nextInt(UserDataManager.getNextDungeonLevel() / 10 + 2) + 1;
+
+		generateRandomMoster(num);
+		return monsterList;
+	}
+
+	private static void generateRandomMoster(int size) {
+		for (int i = 0; i < size; i++) {
+			int num = r.nextInt(MONS.length);
+			String className = PATE + MONS[num];
+			try {
+				Class<?> cls = Class.forName(className);
+				Monster monster = (Monster) cls.getConstructor().newInstance();
+				monsterList.add(monster);
+			} catch (Exception e) {
+				return;
+			}
+		}
+	}
 }
