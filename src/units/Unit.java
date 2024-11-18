e9a196130578ee36e5346217fbbc0d424adb2aa8
SG5143 <sun5143a@gmail.com>
2024-11-18 14:54:43 +0900

    Add common members and getter/setter to Unit class

--- src/units/Unit.java
+++ src/units/Unit.java
@@ -1,5 +1,33 @@
 package units;
 
-public class Unit {
+public abstract class Unit {
+	protected int level;
+	protected String name;
+	protected int att;
+	protected int maxHp;
+	protected int hp;
 
+	public Unit() {}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAtt() {
+		return att;
+	}
+
+	public int getMaxHp() {
+		return maxHp;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public abstract void attack(Unit target);
 }
