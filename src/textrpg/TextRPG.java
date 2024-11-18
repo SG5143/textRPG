5a9f7e1027322038b1c6020c54bea4cb6f2f8939
SG5143 <sun5143a@gmail.com>
2024-11-18 11:33:36 +0900

    Add main stage navigation on start and system exit on command

--- src/textrpg/TextRPG.java
+++ src/textrpg/TextRPG.java
@@ -1,5 +1,60 @@
 package textrpg;
 
-public class TextRPG {
+import manager.IOManager;
+import stage.MainStage;
 
+public class TextRPG {
+	private MainStage mainStage;
+	private boolean isRun;
+
+	private TextRPG() {
+		this.mainStage = new MainStage();
+		this.isRun = true;
+	}
+
+	private static TextRPG instance = new TextRPG();
+
+	public static TextRPG getInstance() {
+		return instance;
+	}
+
+	public void run() {
+		printMainMenuGuide();
+
+		while (true) {
+			String command = IOManager.inputString("여기에 입력해 >> ");
+
+			if (command.equals("시작"))
+				isRun = mainStage.activate();
+			else if (command.equals("종료"))
+				isRun = false;
+
+			if (!isRun) {
+				printExitMessage();
+				break;
+			}
+		}
+	}
+
+	private void printMainMenuGuide() {
+		String msg = """
+				=============================
+				=         TEXT RPG          =
+				=    시작하려면 "시작"을    =
+				=    종료하려면 "종료"를    =
+				=       입력해주세요.       =
+				=============================
+				""";
+		IOManager.printString(msg);
+	}
+
+	private void printExitMessage() {
+		String msg = """
+
+				=============================
+				=   Text RPG를 종료합니다   =
+				=============================
+				""";
+		IOManager.printString(msg);
+	}
 }
