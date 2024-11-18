1c99b7412cfe96618e9cdc61a0b978a701ce70c7
SG5143 <sun5143a@gmail.com>
2024-11-18 10:46:31 +0900

    Create I/O management class

---
+++ src/manager/IOManager.java
@@ -0,0 +1,46 @@
+package manager;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class IOManager {
+	public static StringBuilder sb;
+	public static BufferedReader br;
+	public static BufferedWriter bw;
+
+	private IOManager() {
+		sb = new StringBuilder();
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+
+	private static IOManager instance = new IOManager();
+
+	public static IOManager getInstance() {
+		return instance;
+	}
+
+	public static void printLine(String msg) {
+		sb.setLength(0);
+		sb.append(msg);
+		try {
+			bw.append(sb);
+			bw.flush();
+		} catch (IOException e) {
+			// 
+		}
+	}
+	
+	public static String inputString(String msg) {
+		printLine(msg);
+		try {
+			String input = br.readLine();
+			return input;
+		} catch (IOException e) {
+			return "";
+		}
+	}
+}
